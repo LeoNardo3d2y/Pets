15f94f936bdc1def5533501dd7f664d4ef8fdd66
LeoNardo3d2y <leonardosantos67@hotmail.com>
2024-04-28 12:41:41 -0300

    add um class

--- ClassesPets/src/models/Ong.java
+++ ClassesPets/src/models/Ong.java
@@ -3,6 +3,16 @@ package models;
 public class Ong extends Usuario   {
 
 	private String cnpj;
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 	public String getCnpj() {
 		return cnpj;
